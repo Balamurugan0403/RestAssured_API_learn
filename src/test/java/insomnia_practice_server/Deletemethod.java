578dbadb43592b46bcf5934f183dd5addcb67027
Balamurugan <murugan020325@gmail.com>
2026-06-17 08:03:52 +0530

    Refactor createAndDelete test method for clarity

--- src/test/java/insomnia_practice_server/Deletemethod.java
+++ src/test/java/insomnia_practice_server/Deletemethod.java
@@ -1,26 +1,38 @@
 package insomnia_practice_server;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.testng.annotations.Test;
+
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+
 public class Deletemethod {
 
-@Test
-public void createAndDelete() {
+	@Test
+	public void createAndDelete() {
 
-	Map<String, Object> payload = new HashMap<>();
+		Map<String, Object> payload = new HashMap<>();
 
-	payload.put("name", "Temp User");
-	payload.put("email", "temp@test.com");
-	payload.put("company", "Test");
+		payload.put("name", "Temp User");
+		payload.put("email", "temp@test.com");
+		payload.put("company", "Test");
 
-	Response createResponse = RestAssured.given().contentType(ContentType.JSON).body(payload).when()
-			.post("http://localhost:3000/trainees");
-	String id = createResponse.jsonPath().getString("id");
-	System.out.println("Created ID: " + id);
-	Response deleteResponse = RestAssured.given().when().delete("http://localhost:3000/trainees/" + id);
-	deleteResponse.then().statusCode(200);
+		Response createResponse = RestAssured.given().contentType(ContentType.JSON).body(payload).when()
+				.post("http://localhost:3000/trainees");
 
-	System.out.println("Deleted ID: " + id);
-}
+		createResponse.then().statusCode(201);
+
+		String id = createResponse.jsonPath().getString("id");
+
+		System.out.println("Created ID: " + id);
+
+		Response deleteResponse = RestAssured.given().when().delete("http://localhost:3000/trainees/" + id);
+
+		deleteResponse.then().statusCode(200);
+
+		System.out.println("Deleted ID: " + id);
+	}
 }
